c981978ffd7902153fe686522baf11800ef50c97
Toby Hodgkinson <toby@salamandersoft.co.uk>
2023-02-10 20:27:10 +0000

    Removed '[SerializeField]' from private fields.

--- Assets/Scripts/Bacteria.cs
+++ Assets/Scripts/Bacteria.cs
@@ -1,10 +1,6 @@
-using UnityEngine;
-
 public class Bacteria : PlayerUnit
 {
-	[SerializeField]
 	private static readonly short _maxHitPoints = 8;
-	[SerializeField]
 	private static readonly short _maxMovementPoints = 5;
 
     private void Start()
