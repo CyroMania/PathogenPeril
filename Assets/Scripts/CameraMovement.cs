ad845d29dd640f1eba84f14bbde2f19eb8d37449
Toby Hodgkinson <toby@salamandersoft.co.uk>
2023-02-12 00:43:14 +0000

    Added 'movementVector' vector2 to monitor each control input and move in appropriate direction.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    
+    [SerializeField]
+    private float _speedDampener = 12f;
+
+    private void Update()
+    {
+        Vector2 movementVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementVector += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementVector += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementVector += Vector2.right;
+        }
+
+        transform.Translate(movementVector / _speedDampener);
+    }
 }
