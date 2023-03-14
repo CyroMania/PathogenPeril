f52cbc3ad000605d2e434dbd2744e4edcd9c6505
Toby Hodgkinson <tobyhodgkinson@gmail.com>
2023-03-14 22:35:40 +0000

    Set end tiles to Goal tiles.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -18,14 +18,20 @@ public class Grid : MonoBehaviour
     {
         List<Tile> tiles = FindObjectsOfType<Tile>().ToList();
 
-        tile.gameObject.layer = 3;
+        tile.gameObject.layer = LayerMask.NameToLayer("Tile");
 
         for (float x = 0; x < gridWidth; x++)
         {
             for (float y = 0; y < gridHeight; y++)
             {
                 Tile clone = Instantiate(tile, new Vector3(x, y, transform.position.z), Quaternion.identity, transform);
-                clone.name = "Tile (" + x + y + ")";
+                clone.name = "Tile (" + x + ":" + y + ")";
+
+                if (y == gridHeight - 1)
+                {
+                    clone.Goal = true;
+                }
+
                 tiles.Add(clone);
             }
         }
