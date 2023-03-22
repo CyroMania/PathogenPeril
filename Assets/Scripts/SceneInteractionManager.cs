f3cdf8c242d4e60802c324958cdab02a1eaf6e5b
Toby Hodgkinson <tobyhodgkinson@gmail.com>
2023-03-22 15:48:44 +0000

    Refactored 'SceneInteractionManager' to preemptively hold the buildIndex of the current scene when it loads.

--- Assets/Scripts/SceneInteractionManager.cs
+++ Assets/Scripts/SceneInteractionManager.cs
@@ -3,15 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneInteractionManager : MonoBehaviour
 {
+    private static int _currentSceneBuildIndex;
+
+    private void Start()
+    {
+        _currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
     public static void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(_currentSceneBuildIndex + 1);
     }
 
     //This is needed for the retry button functionalitys
     public static void ReloadCurrentLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(_currentSceneBuildIndex);
     }
 
     public static void LoadMainMenu()
