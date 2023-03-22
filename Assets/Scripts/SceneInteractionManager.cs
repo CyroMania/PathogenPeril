0a79823cc5926ade66837b58ee54c4fb4332c9d4
Toby Hodgkinson <tobyhodgkinson@gmail.com>
2023-03-22 14:53:09 +0000

    Added 'PauseMenu' object with 3 buttons, Continue, Retry & Main Menu to the main scene.

--- Assets/Scripts/SceneInteractionManager.cs
+++ Assets/Scripts/SceneInteractionManager.cs
@@ -8,6 +8,13 @@ public class SceneInteractionManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+
+    public static void LoadMainMenu()
+    {
+        //The first scene we build is the Main Menu which is why we load 0.
+        SceneManager.LoadScene(0);
+    }
+
     public static void QuitGame()
     {
         Application.Quit();
