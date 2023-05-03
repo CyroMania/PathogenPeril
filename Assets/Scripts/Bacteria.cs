5e35a8fa715e2461d0de9b1ac61715ce45f9c6f7
Toby Hodgkinson <tobyhodgkinson@gmail.com>
2023-05-03 08:41:16 +0100

    Added comments to 'Bacteria' class.

--- Assets/Scripts/Bacteria.cs
+++ Assets/Scripts/Bacteria.cs
@@ -9,12 +9,15 @@ public class Bacteria : PlayerUnit
     private static readonly short _visibilityRange = 6;
 
     private const string DivideBtnName = "DivideBtn";
+    private const string EnergyProperty = "Energy";
+
     private Button _divideBtn;
 
     private void Start()
     {
         base.Init(_maxHitPoints, _maxMovementPoints, _visibilityRange);
 
+        //Because we need this behaviour is triggered by the UI button, we need to add this trigger dynamically as a listener for each unit that spawns.
         _divideBtn = GameObject.Find(DivideBtnName).GetComponent<Button>();
         _divideBtn.onClick.AddListener(Divide);
 
@@ -25,38 +28,53 @@ public class Bacteria : PlayerUnit
         }
     }
 
+    /// <summary>
+    /// The main behaviour that bacteria use to create copies of themselves.
+    /// </summary>
     private void Divide()
     {
+        //The divide command requires all energy and we must only activate it on the selected unit because otherwise all player units would divide.
         if (Selected && MovementPoints == _maxMovementPoints)
         {
             List<Tile> acceptableTiles = new List<Tile>();
 
             foreach (Tile tile in CurrentTile.NeighbouringTiles)
             {
+                //Inline with one of the requirements, no units can inhabit the same tile.
                 if (!tile.Inhabited)
                 {
                     acceptableTiles.Add(tile);
                 }
             }
 
+            //Randomly choose a tile to make it feel more natural.
             Tile chosenTile = acceptableTiles[Random.Range(0, acceptableTiles.Count)];
             Bacteria clone = Instantiate(this, transform.position, Quaternion.identity);
+            //Spawns new stat bars for the cloned unit.
             GameObject.Find("Canvas").GetComponent<UnitUI>().CreateNewStatBars(clone);
             JustDivided = true;
             clone.name = "Bacteria";
-            clone.Clone = true; 
+            clone.Clone = true;
+
+            //Begins the coroutine to animate the movement.
             StartCoroutine(DivideToNewTile(clone, chosenTile.transform.position + TileMovement.UnitLayer));
  
             MovementPoints = 0;
             ResetAllTiles(ignoredProps: new string[] { nameof(Tile.Visible), nameof(Tile.Goal) });
             CurrentTile.Current = true;
             UI.CheckButtonsUsable(MovementPoints, MaxMovementPoints, IsFullySurrounded());
-            UnitUI.UpdateStatBarValue(this, "Energy");
+            UnitUI.UpdateStatBarValue(this, EnergyProperty);
             return;
         }
     }
 
-    IEnumerator<Vector3> DivideToNewTile(Bacteria clone, Vector3 destination)
+    /// <summary>
+    /// The animation for moving a newly spawned unit to the current unit's neighbour tile.
+    /// </summary>
+    /// <param name="clone">The original unit to copy.</param>
+    /// <param name="destination">The new tile to clone the unit to.</param>
+    /// <returns>An interpolated position vector.</returns>
+    private IEnumerator<Vector3> DivideToNewTile(Bacteria clone, Vector3 destination)
     {
         while (Vector3.Distance(clone.transform.position, destination) > 0.03f)
         {
@@ -67,7 +85,7 @@ public class Bacteria : PlayerUnit
 
         clone.transform.position = destination;
         clone.CurrentTile = TileMovement.CalculateCurrentTile(clone);
-        UnitUI.UpdateStatBarValue(clone, "Energy");
+        UnitUI.UpdateStatBarValue(clone, EnergyProperty);
         UnitUI.UpdateStatBarPositions(clone, Camera.main.WorldToScreenPoint(destination));
         TileMovement.FindVisibleTiles(CurrentTile, new Queue<Tile>(), 1, Visibility);
     }
