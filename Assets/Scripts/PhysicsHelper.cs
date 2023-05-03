10d3fdb7e16ad921acace24c8262151c9e12b933
Toby Hodgkinson <tobyhodgkinson@gmail.com>
2023-05-03 08:16:53 +0100

    Added comments to PhysicsHelper.

--- Assets/Scripts/PhysicsHelper.cs
+++ Assets/Scripts/PhysicsHelper.cs
@@ -2,6 +2,12 @@
 
 public static class PhysicsHelper
 {
+    /// <summary>
+    /// Creates a Unity Raycast.
+    /// </summary>
+    /// <param name="targetLayer">The layer we want to hit colliders in.</param>
+    /// <param name="raycastOrigin">The location where the raycast start.</param>
+    /// <returns>A RaycastHit2D with the first collider hit on that layer.</returns>
     internal static RaycastHit2D GenerateRaycast(string targetLayer, Vector3 raycastOrigin)
     {
         int unitMask = 1 << LayerMask.NameToLayer(targetLayer);
